4d2bafce615941c482d820ac4023b653f49e161f
gr8fuldwg <gr8fuldwg@gmail.com>
2021-02-20 08:40:13 -0500

    successfully retrieved JSON data from 'inspirational quotes' API in console after API creator updated backend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,247 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+// import Quote from "./helpers.js/quote.js";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+// API URL variables
+const CHUCK_API = "https://api.chucknorris.io/jokes/random";
+const QUOTE_API = "https://quote-garden.herokuapp.com/api/v3/quotes/random";
+const KANYE_API = "https://api.kanye.rest";
+const DOGS_API = "https://dog.ceo/api/breeds/image/random";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      on: false,
+      imageURL: "",
+      showContent: true,
+      error: null,
+    };
+
+    // Binding methods
+    this.handle = this.handle.bind(this);
+    this.quote = this.quote.bind(this);
+    this.kanye = this.kanye.bind(this);
+    this.dogs = this.dogs.bind(this);
+    this.toggleContent = this.toggleContent.bind(this);
+  }
+
+  makeGray() {
+    var k = document.getElementById("kGray");
+    var color = k.style.color;
+    k.addEventListener("click", function () {
+      // this function executes whenever the user clicks the button
+      console.log("yes");
+      color = k.style.color = color === "grey";
+    });
+  }
+
+  handleClick() {
+    console.log("this is:", this);
+  }
+
+  toggleContent(event) {
+    event.preventDefault();
+    this.setState({
+      showContent: !this.state.showContent,
+    });
+  }
+
+  // Dog Photo Axios GET request
+  componentDidMount() {
+    axios
+      .get(DOGS_API)
+      .then((response) => {
+        // console.log(response.data);
+        this.setState({ imageURL: response.data.message });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  dogs() {
+    axios
+      .get(DOGS_API)
+      .then((response) => {
+        const { message } = response.data;
+        this.setState({
+          message: message,
+          imageURL: response.data.message,
+          error: null,
+        });
+        console.log(response.data);
+      })
+
+      .catch((err) => {
+        this.setState({
+          error: err,
+        });
+        console.log("error");
+      });
+  }
+
+  // 'Inspirational Quotes' Axios GET method
+  quote() {
+    axios
+      .get(QUOTE_API)
+      .then((response) => {
+        // const { quotes } = response.data.data[0].quoteText;
+        const { quoteText } = response.data.data[0].quoteText;
+        const { quoteAuthor } = response.data.data[0].quoteAuthor;
+
+        console.log(response.data.data[0].quoteText);
+        console.log(response.data.data[0].quoteAuthor);
+
+        this.setState({
+          // quotes: quotes,
+          quoteText: quoteText,
+          quoteAuthor: quoteAuthor,
+          hyphen: " -",
+          error: null,
+        });
+      })
+
+      .catch((err) => {
+        this.setState({
+          error: err,
+        });
+        console.log("error");
+      });
+  }
+
+  // Kanye Quotes Axios GET method
+  kanye() {
+    axios
+      .get(KANYE_API)
+      .then((response) => {
+        const { quote } = response.data;
+        this.setState({
+          quote: quote,
+          name: " - Kanye West",
+          error: null,
+        });
+      })
+
+      .catch((err) => {
+        this.setState({
+          error: err,
+        });
+        console.log("error");
+      });
+  }
+
+  // Chuck Joke Axios GET method
+  handle() {
+    axios
+      .get(CHUCK_API)
+      .then((response) => {
+        const { value } = response.data;
+        this.setState({
+          value: value,
+          error: null,
+        });
+        console.log(this.state.value);
+      })
+
+      .catch((err) => {
+        this.setState({
+          error: err,
+        });
+        console.log("error");
+      });
+  }
+
+  // Render API's to DOM via buttons
+  render() {
+    const { imageURL } = this.state;
+    const { showContent } = this.state;
+    return (
+      <div className="body">
+        <h1 className="head">A wise dog once said...</h1>
+
+        <img id="dog" src={imageURL} alt="dog" />
+
+        {/* *** BUTTONS *** */}
+        <section className="d-flex ml-2 mt-2 mr-2 mb-2" id="buttons">
+          {/* Chuck */}
+          <button
+            id="chaz"
+            type="button"
+            className="chux chuckbutton btn btn-outline-danger"
+            onClick={this.handle}
+          >
+            Chaz
+          </button>
+
+          {/* Inspiration */}
+          <button
+            id="inspiration"
+            type="button"
+            className="inspire btn btn-dark "
+            onClick={this.quote}
+          >
+            Inspire
+          </button>
+
+          {/* Kanye */}
+          <button
+            type="button"
+            className="kanye btn btn-outline-warning"
+            onClick={this.kanye}
+          >
+            Kanye
+          </button>
+
+          <button
+            type="button"
+            className="btn btn-outline-primary"
+            onClick={this.dogs}
+          >
+            Dogs
+          </button>
+        </section>
+        {/* End Buttons */}
+
+        {/* **** QUOTES **** */}
+
+        {/* Inspiration */}
+        <h1 className="quote d-flex pt-4">{this.state.quoteAuthor}</h1>
+        <h1>{this.state.res}</h1>
+
+        <h3 className="fade1 name d-flex pb-4">
+          {this.state.hyphen}
+          {this.state.quoteAuthor}
+        </h3>
+
+        {/* Kanye */}
+        {showContent === true ? (
+          <h1 onClick={this.toggleContent} className="kanye d-flex pt-4">
+            {this.state.quote}
+          </h1>
+        ) : (
+          ""
+        )}
+        <button
+          type="button"
+          id="kGray"
+          className="btn btn-outline-primary"
+          onClick={() => this.makeGray()}
+        >
+          <h3
+            onClick={this.toggleContent.bind(this)}
+            className="grey-after fade2 test kanye d-flex pb-4"
+          >
+            {this.state.name}
+          </h3>
+        </button>
+
+        {/* Chuck */}
+        <h1 className="fade3 chaz d-flex pt-4 pb-4">{this.state.value}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/helpers.js/quote.js
@@ -0,0 +1,28 @@
+// import React, { useState } from "react";
+// import Axios from "axios";
+// // import "./App.css";
+
+// function Quote() {
+//   const [quote, setQuote] = useState("");
+
+//   const getQuote = () => {
+//     Axios.get("https://quote-garden.herokuapp.com/api/v3/quotes/random").then(
+//       (response) => {
+//         console.log(response.data.data[0].quoteText);
+//         console.log(response.data.data[0].quoteAuthor);
+
+//         const respsonse = response;
+//         const quotes = response.data.data[0];
+//       }
+//     );
+//   };
+
+// //   return (
+// //     <div>
+// //       <button onClick={getQuote}>Press for Quote</button>
+// //       {quote}
+// //     </div>
+// //   );
+// }
+
+// export default Quote;
