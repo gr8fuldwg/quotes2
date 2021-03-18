0baf461dafa2a1fe2ea97d4afcfe1a6d0f14c9b8
gr8fuldwg <gr8fuldwg@gmail.com>
2021-03-17 20:49:39 -0400

    Updates

---
+++ precache-manifest.a7b6e47cfc4af8ebd78638b6d13f9383.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd39ed44b41d6f9b84c589c552e0c09",
+    "url": "/quotes2/index.html"
+  },
+  {
+    "revision": "2dc76687c73640512027",
+    "url": "/quotes2/static/css/main.6a047dd7.chunk.css"
+  },
+  {
+    "revision": "87575513f9b9a36aec4c",
+    "url": "/quotes2/static/js/2.084fbad7.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/quotes2/static/js/2.084fbad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dc76687c73640512027",
+    "url": "/quotes2/static/js/main.27e80b69.chunk.js"
+  },
+  {
+    "revision": "dafdcac998e5f772fd6a",
+    "url": "/quotes2/static/js/runtime-main.c92a2d4f.js"
+  }
+]);
